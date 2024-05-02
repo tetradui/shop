3c678d13b7b197609f35afdd107a2482f46b1fa6
tetradui <kutmanalown@gmail.com>
2024-05-02 15:10:28 +0600

    added urls acc

--- account/urls.py
+++ account/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import * 
+from .views import RegisterView, ActivateView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view()),
+    path('activate/<str:email>/<str:activation_code>/', ActivateView.as_view(), name='activate'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+]
